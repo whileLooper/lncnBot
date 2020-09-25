bcf379828f260d93fe38a15a928ece497f1c645b
bo.chen <bo.chen@svuslp00615.lan>
2020-09-25 17:16:12 -0400

    debug copy to clipboard function

--- public/script.js
+++ public/script.js
@@ -13,17 +13,28 @@ document.querySelector('button[id="copyBtn"]').addEventListener('click', (e) => 
     .catch((err) => {
       console.log(err)
       document.getElementById('result').textContent = `Error: ${err.toString()}`
-  });;
+    });;
 })
 
 const copyToClipboard = str => {
   const el = document.createElement('textarea');
   el.value = str;
-  el.setAttribute('readonly', '');
-  el.style.position = 'absolute';
-  el.style.left = '-9999px';
-  document.body.appendChild(el);
-  el.select();
+  var oldContentEditable = el.contentEditable,
+    oldReadOnly = el.readOnly,
+    range = document.createRange();
+
+  el.contentEditable = true;
+  el.readOnly = false;
+  range.selectNodeContents(el);
+
+  var s = window.getSelection();
+  s.removeAllRanges();
+  s.addRange(range);
+
+  el.setSelectionRange(0, 999999); // A big number, to cover anything that could be inside the element.
+
+  el.contentEditable = oldContentEditable;
+  el.readOnly = oldReadOnly;
+
   document.execCommand('copy');
-  document.body.removeChild(el);
 };
