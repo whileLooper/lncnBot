4bcf9a887b0ff4414e071bf7106dd6db8599acc2
bo.chen <bo.chen@svuslp00615.lan>
2020-09-25 16:22:14 -0400

    updated gitignore

---
+++ public/script.js
@@ -0,0 +1,11 @@
+const options = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json; charset=utf-8"
+  },
+  body: JSON.stringify({
+    pageToScreenshot: "https://lncn.org/"
+  })
+};
+
+fetch("/.netlify/functions/copy-node", options);
