e397a6d37f46a98ef3f038e3a60e33c87141850d
bo.chen <bo.chen@svuslp00615.lan>
2020-09-25 16:56:07 -0400

    updated html button script

--- public/script.js
+++ public/script.js
@@ -1,11 +1,20 @@
-// const options = {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json; charset=utf-8"
-//   },
-//   body: JSON.stringify({
-//     pageToScreenshot: "https://lncn.org/"
-//   })
-// };
 
-fetch("/.netlify/functions/copy-node", options);
+document.querySelector('button[type="submit"]').addEventListener('click', (e) => {
+  fetch("/.netlify/functions/copy-node", options)
+    .then((res) => res.json())
+    .then((res) => {
+      copyToClipboard(res.message)
+    });
+})
+
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
