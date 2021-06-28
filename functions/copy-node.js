ffb83d4651d8b753289780c39a27abf88e0b7cfd
Bo Chen <bochentheone@gmail.com>
2021-06-28 14:33:06 +0000

    updated function headers

--- functions/copy-node.js
+++ functions/copy-node.js
@@ -1,5 +1,10 @@
 const chromium = require('chrome-aws-lambda');
 const buttonSelector = "body > div > div > div> div > div.layout-left-right.el-row > div.boxes-wrapper.layout-main.el-col.el-col-17.el-col-xs-24.el-col-sm-24.el-col-md-17 > div.ssr-list-wrapper.base-box > div.ssr-btn-bar > button";
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+};
 
 exports.handler = async (event, context) => {
 
@@ -32,6 +37,7 @@ exports.handler = async (event, context) => {
   await browser.close();
   return {
     statusCode: 200,
+    headers,
     body: JSON.stringify({ 
         message: copiedText,
         seletor: buttonSelector
